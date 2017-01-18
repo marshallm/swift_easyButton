26605f19a05426a8507f5e04f56352cb0475719f
Mike <mtymarshall@live.com>
2017-01-17 19:01:15 -0800

    Adds Source

---
+++ easyButton/easyButton/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  easyButton
+//
+//  Created by Mike on 1/17/17.
+//  Copyright © 2017 edu.self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showMessage() {
+        
+        let alertController = UIAlertController(title: "Request Submitted", message: "Thanks", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+
+
+}
+
